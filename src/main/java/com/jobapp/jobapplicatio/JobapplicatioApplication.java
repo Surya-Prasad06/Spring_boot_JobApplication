4d86de3d1a0bf560cf6ab29e61dceb21e81031f6
Surya-Prasad06 <suryaprasadsadige007@gmail.com>
2025-09-10 11:12:48 +0530

    first commit

---
+++ src/main/java/com/jobapp/jobapplicatio/JobapplicatioApplication.java
@@ -0,0 +1,13 @@
+package com.jobapp.jobapplicatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapplicatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapplicatioApplication.class, args);
+	}
+
+}
